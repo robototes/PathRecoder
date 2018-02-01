c1a7306dcf2203cd8833815b01bf852091ba938c
Robbie Robototes <robototes2412@gmail.com>
2018-01-31 16:19:20 -0800

    Added Trajectory constructor.

--- src/org/usfirst/frc/team2412/robot/trajectory/Trajectory.java
+++ src/org/usfirst/frc/team2412/robot/trajectory/Trajectory.java
@@ -1,12 +1,23 @@
 package org.usfirst.frc.team2412.robot.trajectory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This class represents a trajectory that the robot can follow (for example, driving in a straight line).
- * Each Trajectory instance holds an array of Setpoint instances.
+ * Each Trajectory instance holds an ArrayList of Setpoint instances.
  * 
  * @author Robototes
  *
  */
 
 public class Trajectory {
+	private List<Setpoint> setpoints = new ArrayList<Setpoint>();
+	
+	/**
+	 * Constructor. Creates a Trajectory from a List of Setpoints.
+	 */
+	public Trajectory(List<Setpoint> setpoints) {
+		this.setpoints = setpoints;
+	}
 }
