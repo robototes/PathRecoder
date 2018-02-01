2d408f89ff3a64ffa6fe7716409b7ddd78c2f5ac
Robbie Robototes <robototes2412@gmail.com>
2018-01-31 17:50:54 -0800

    Write the recorded path to a file.

--- src/org/usfirst/frc/team2412/robot/commands/RecordPathCommand.java
+++ src/org/usfirst/frc/team2412/robot/commands/RecordPathCommand.java
@@ -1,10 +1,17 @@
 package org.usfirst.frc.team2412.robot.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.usfirst.frc.team2412.robot.Robot;
+import org.usfirst.frc.team2412.robot.trajectory.Setpoint;
+import org.usfirst.frc.team2412.robot.trajectory.Trajectory;
 
 public class RecordPathCommand extends CommandBase {
+	private List<Setpoint> points = new ArrayList<Setpoint>();
 	public RecordPathCommand() {
 		requires(driveBase);
+		requires(fileSystem);
 	}
 	@Override
 	protected boolean isFinished() {
@@ -13,6 +20,19 @@ public class RecordPathCommand extends CommandBase {
 	
 	protected void execute() {
 		System.out.println("Recording path...");
-		driveBase.drive(Robot.m_oi.stick);
+		
+		double velocity = -Robot.m_oi.stick.getY();
+		double angle = Robot.m_oi.stick.getTwist();
+		
+		System.out.println("Velocity: " + velocity);
+		System.out.println("Angle: " + angle);
+		
+		points.add(new Setpoint(velocity, angle));
+		
+		driveBase.drive(velocity, angle, true);
+	}
+	
+	protected void end() {
+		fileSystem.writeTrajectory(new Trajectory(points), 0);
 	}
 }
