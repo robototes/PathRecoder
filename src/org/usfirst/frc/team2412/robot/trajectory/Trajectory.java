8bfc1e7c40e81b6b4ba093c60fb22eb1d8e9fa14
Robbie Robototes <robototes2412@gmail.com>
2018-01-31 16:01:01 -0800

    Added (empty) Trajectory class.

---
+++ src/org/usfirst/frc/team2412/robot/trajectory/Trajectory.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team2412.robot.trajectory;
+
+/**
+ * This class represents a trajectory that the robot can follow (for example, driving in a straight line).
+ * Each Trajectory instance holds an array of Setpoint instances.
+ * 
+ * @author Robototes
+ *
+ */
+
+public class Trajectory {
+}
